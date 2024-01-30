9873d75e5be59fa52dfa8de7dc5baba3776294d3
Sebastian Vivas <sebastianvivas@mac-mini.lan>
2024-01-29 19:44:53 -0500

    Added MongoDB credentials

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require('mongodb');
+
+// The database name is "notesApp" and the collection is "notes".
+// username: developer
+// password: ABC123
+const uri = "mongodb+srv://developer:ABC123@database.rtodijr.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = 3001;
 app.listen(PORT)
