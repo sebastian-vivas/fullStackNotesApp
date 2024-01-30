af6be8a11bfe89cd3d56cfdb6130fca61a8326ed
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2024-01-29 19:40:08 -0500

    sections for backend

--- server/server.js
+++ server/server.js
@@ -4,3 +4,24 @@ const app = express()
 const PORT = 3001;
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
+// Section for deleting notes
+
+
+// Sections for posting notes
+
+
+// Section for getting notes
+
+
+// Section for updating notes (text)
+
+
+// Section for updating notes (pin)
+
+
+// Section for updating notes
+
+
+// Add marker that note is important
+
