9f8f243b0c5a3f651e321e66caf113245f73e42b
Sebastian Vivas <sebastianvivas@mac-mini.lan>
2024-01-30 20:49:17 -0500

    Updated client and server with api's and POST

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,63 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    note: "",
+    important: false,
+    pinned: false,
+  });
+
+  const updateForm = (value) => {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  const onSubmit  = async (e) => {
+    e.preventDefault();
+    //  When a post request is sent to the create url, we'll add a new record to the database.
+    const newNote = { ...form };
+     await fetch("http://localhost:3001/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newNote),
+    })
+    .catch(error => {
+      window.alert(error);
+      return;
+    });
+     setForm({ note: "", important: false, pinned: false });
+    navigate("/");
+  }
 
   return (
-    <>
+    <div>
+      <h3>Create New Note</h3>
+    <form onSubmit={onSubmit}>
+      <div>
+        <input value={form.note} onChange={(e) => updateForm({ note: e.target.value })} />{" "}
+        <button type="submit">add note</button>
+      </div>
+    </form>
+      {notes.map((note) => (
+        <div>
+          <p key={note._id}>
+            {note.note}{" "}
+            {!note.important && <button>star</button>}
+            {note.important && <button>starred</button>}{" "}
+            {!note.pinned && <button>pin</button>}
+            {note.pinned && <button>pinned</button>}
+          </p>
+        </div>
+        ))}
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const { MongoClient } = require('mongodb');
 
 // The database name is "notesApp" and the collection is "notes".
@@ -7,18 +8,31 @@ const { MongoClient } = require('mongodb');
 // password: ABC123
 const uri = "mongodb+srv://developer:ABC123@database.rtodijr.mongodb.net/?retryWrites=true&w=majority";
 
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
 const PORT = 3001;
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
 
 // Section for deleting notes
-
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id != id)
+    response.status(204).end()
+})
 
 // Sections for posting notes
-
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
 
 // Section for getting notes
-
+app.get('/api/notes', (request, response) => {
+    response.json
+})
 
 // Section for updating notes (text)
 
@@ -30,4 +44,5 @@ console.log(`Server running on port ${PORT}`)
 
 
 // Add marker that note is important
+
 
